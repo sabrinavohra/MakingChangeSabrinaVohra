bb637502611f44bc92cb67ac44b1b7c3b7f46724
sabrinavohra <sabrina.vohra@menloschool.org>
2025-01-27 09:54:58 -0800

    Brainstorm and pseudocode.

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -4,6 +4,7 @@ Name: Sabrina Vohra
 |:-----|:---------------:|-------------------------------------------------------------------------------------------:|
 | 1/26 | 12:25 - 12:55pm |           I began thinking through the project and writing pseudocode for my primary idea. |
 | 1/27 |  9:05 - 9:35am  | I brainstormed new ideas alongside my classmates and wrote basic pseudocode for each idea. |
+| 1/27 |  9:35 - 9:55am  |                                          I continued brainstorming and writing pseudocode. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MakingChange.java
+++ src/MakingChange.java
@@ -21,11 +21,14 @@ public class MakingChange {
                 // Start by taking the largest coin and dividing the target by it the appropriate amount of times
                 // Then, split into smaller values and do the appropriate number of each to reach target
             // Find which coins fit into the other coins to create their values
-            // Bottom up solution:
-                // Start with the smallest coins and then start adding larger coins to the solution
-            // Top down solution:
-                // Start with the largest coins and then start replacing them with smaller coins
-            // How to perform the two above solutions recursively / efficiently?
+            // How to perform the two solutions recursively / efficiently?
+                // Bottom up solution:
+                    // Start with the smallest coins and then start adding larger coins to the solution
+                // Top down solution:
+                    // Start with the largest coins and then start replacing them with smaller coins
+
+
+
 
         return (long) target % coins.length * (2L * target);
     }
