e55cb0996334f31a417a92f4d3ac4ee52625c7d4
sabrinavohra <sabrina.vohra@menloschool.org>
2025-02-06 15:00:02 -0800

    Started memoization.

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -9,6 +9,7 @@ Name: Sabrina Vohra
 | 2/1  | 11:00 - 11:30pm |                                                                                                                        I continued working through the code using my pseudocode and recursive ideas to find base cases. |
 | 2/1  | 11:30 - 11:50pm |                                                                                                                                                    I continued thinking through base cases for the recursive algorithm. |
 | 2/3  | 10:45 - 11:15am |                                                                                                                                                                      We looked on the slides and brainstormed in class. |
+| 2/6  |  2:45 - 3:00pm  |                                                            I started working through the process on the slides for memoization. I am getting an out of bounds error and think I am not properly checking the 2-d array. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MakingChange.java
+++ src/MakingChange.java
@@ -13,54 +13,28 @@ public class MakingChange {
      * TODO: Complete this function, countWays(), to return the number of ways to make change
      *  for any given total with any given set of coins.
      */
-    private static int[] possiblities;
+
+    public static int[] coin;
+    public static int[][] memo;
     public static long countWays(int target, int[] coins) {
-        possiblities = new int[target];
-        for(int i = 0; i < target; i++) {
-            ways(target, coins, 0);
-        }
-
-
-
-            // Take stock of the coins in list
-
-        //Ideas:
-        // Create recursive algorithm:
-        // Start by taking the largest coin and dividing the target by it the appropriate amount of times
-        // Then, split into smaller values and do the appropriate number of each to reach target
-        // Find which coins fit into the other coins to create their values
-        // How to perform the two solutions recursively / efficiently?
-        // Bottom up solution:
-        // Start with the smallest coins and then start adding larger coins to the solution
-        // Top down solution:
-        // Start with the largest coins and then start replacing them with smaller coins
-
-        // Find way to join together the two ways of solving smaller problems
-        // 1. Lessening the number of the target value
-        // 2. Removing certain coins to solve the problem with less coins
-
-        return ways(target, coins, 0);
-
-        //return (long) target % coins.length * (2L * target);
+        coin = coins;
+        memo = new int[target][coins.length];
+        return ways(target, 0);
     }
 
-    public static int ways(int target, int[] coins, int currentWays) {
-        // Possible base cases? Possible ways to save number of paths to certain target value / certain number of coins?
-        if (target <= 0 || coins.length <= 1) {
-            return currentWays + 1;
+    public static int ways(int target, int i) {
+
+        if (target == 0) {
+            return 1;
         }
-        if (possiblities[target] != 0) {
-            return possiblities[target];
+        if ((target < 0) || i >= coin.length) {
+            return 0;
         }
-        // How to find combinations that work?
-        currentWays++;
-        possiblities[target] = currentWays;
-        int[] newCoins = new int[coins.length - 1];
-        for(int i = 0; i < newCoins.length; i++) {
-            newCoins[i] = coins[i];
+        if(memo[target][i] != 0) {
+            return  memo[target][i];
         }
-        ways(target - 1, coins, currentWays);
-        ways(target, newCoins, currentWays);
-        return currentWays;
+
+        memo[target][i] = ways(target-coin[i], i) + ways(target, i + 1);
+        return memo[target][i];
     }
 }
