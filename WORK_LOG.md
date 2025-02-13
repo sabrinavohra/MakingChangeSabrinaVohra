f43db334e44d408bc3046a2c462f1f26625d67f5
sabrinavohra <sabrina.vohra@menloschool.org>
2025-02-13 14:10:55 -0800

    Comments

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -12,7 +12,7 @@ Name: Sabrina Vohra
 | 2/6  |  2:45 - 3:00pm  |                                                             I started working through the process on the slides for memoization. I am getting an out of bounds error and think I am not properly checking the 2-d array. |
 | 2/10 | 12:50 - 1:20pm  | I figured out the memoization solution by figuring out that I had to increase the number of rows in my storage table, with Mr. Blick's help. I began working on tabulation by solving a similar way but going bottom up. |
 | 2/12 |  9:35 - 9:50am  |                                                                                                                              I worked on the tabulation approach by trying to use a for-loop to update the array values. |
-| 2/13 |  1:50 - 2:00pm  |                            I worked on my tabulation approach, and realized I was calling the wrong target when I call the method in  my main method. This fixed the problems I was having and made my solution correct. |
-
+| 2/13 |  1:50 - 2:10pm  |             I worked on my tabulation approach, and realized I was calling the wrong target when I call the method in  my main method. This fixed the problems I was having with out of bounds errors. I wrote comments. |
+                                                                                                                                                                                                                                      |
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MakingChange.java
+++ src/MakingChange.java
@@ -7,23 +7,25 @@
  */
 
 public class MakingChange {
-    /**
-     * TODO: Complete this function, countWays(), to return the number of ways to make change
-     *  for any given total with any given set of coins.
-     */
-
+    // Created variables for entire class
     public static int[] coin;
     public static long[][] numWays;
-    public static int total;
+
     public static long countWays(int target, int[] coins) {
-        total = target;
+        // Initializes variables
         coin = coins;
         numWays = new long[target + 1][coins.length];
+        // Calls two methods for either memoization or tabulation approach
+
+        // Comment in the line below for memoization approach
         //return memoWays(target, coins.length - 1);
-        return tabWays(target, 0);
+        // Comment in the line below for tabulation approach
+        return tabWays(target);
     }
 
+    // Completes solution using memoization
     public static long memoWays(int target, int i) {
+        // Base cases to not go out of bounds or repeat same recursive calls multiple times
         if (target == 0) {
             return 1;
         }
@@ -33,27 +35,32 @@ public class MakingChange {
         else if (numWays[target][i] > 0) {
             return numWays[target][i];
         }
+        // Recursively calls to use memoization for using one less coin value in target and one less coin in coins and
+        // updates in table
         numWays[target][i] = memoWays(target-coin[i], i) + memoWays(target, i - 1);
         return numWays[target][i];
     }
 
-    public static long tabWays(int target, int index) {
-        if(index >= coin.length || target < 0 || index < 0) {
-            return 0;
-        }
+    // Method solves using tabulation
+    public static long tabWays(int target) {
+        // Sets all spots with 0 as a value and any coins as equal to 1
         for(int k = 0; k < coin.length; k++) {
             numWays[0][k] = 1;
         }
+        // Goes through all spots in table and updates each one
         for(int m = 1; m <= target; m++) {
             for(int n = 0; n < coin.length; n++) {
+                // Finds lesser value if target is large enough to subtract a coin
                 if ((m - coin[n]) >= 0) {
                     numWays[m][n] += numWays[m - coin[n]][n];
                 }
+                // Finds lesser value if there is another coin of lesser value
                 if ((n - 1) >= 0) {
                     numWays[m][n] += numWays[m][n - 1];
                 }
             }
         }
+        // Returns last spot in table
         return numWays[target][coin.length - 1];
     }
 }
